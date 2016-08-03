bb4b3ab36cf65a936d7f3b0a360b41bcd37ce8dc
James Slone <slonej3@mymail.nku.edu>
2016-08-03 17:15:42 -0400

    adding files

---
+++ CH5/Program.fs
@@ -0,0 +1,9 @@
+﻿(*namespace Test*)
+module Program =
+    open PoliceChase
+    [<EntryPoint>]
+    let main argv =
+        simulation() 
+        printfn "%A" argv
+        0 // return an integer exit code
+  
